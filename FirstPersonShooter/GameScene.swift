6665befc19f68d7dfad1a15e12e545217189a43a
student1 <student1@jh-mba158-19.d214.org>
2017-05-17 09:45:25 -0500

    Initial Commit

---
+++ FirstPersonShooter/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  FirstPersonShooter
+//
+//  Created by student1 on 5/17/17.
+//  Copyright © 2017 John Hersey High School. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+   
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+    
+
+    
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
